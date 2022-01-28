6e215061ca282faa828ee7a9c44eb1c24104755d
Liam Armstrong <ltarmstrong94@gmail.com>
2022-01-28 10:17:24 -0800

    first commit, styling finished

---
+++ app.js
@@ -0,0 +1,9 @@
+const hourHand = document.querySelector('.hour')
+const minuteHand = document.querySelector('.minute')
+const secondHand = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+const toggle = document.querySelector('.toggle')
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
