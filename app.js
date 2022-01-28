8459258c62203d56dc7486bfa195bd76c6a6cf38
Liam Armstrong <ltarmstrong94@gmail.com>
2022-01-28 11:48:08 -0800

    basic clock functionality added

--- app.js
+++ app.js
@@ -7,3 +7,37 @@ const toggle = document.querySelector('.toggle')
 
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',];
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+
+toggle.addEventListener('click', function (e) {
+    const html = document.querySelector('html');
+    html.classList.toggle('dark');
+    if (html.classList.contains('dark')) {
+        e.target.innerText = 'Light Mode'
+    } else {
+        e.target.innerText = 'Dark Mode'
+    }
+});
+
+function setTime() {
+    const time = new Date();
+    const month = time.getMonth();
+    const day = time.getDay();
+    const hours = time.getHours();
+    const clockHours = hours % 12;
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    hourHand.style.transform = `translate(-50%, -100%) rotate(${scale(clockHours, 0, 12, 0, 360)}deg)`;
+    minuteHand.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`;
+    secondHand.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`;
+}
+
+// below is a useful utility function called the scale function...allows us to map a range of numbers to another range of numbers...
+// In this case, it will be mapped with 0-12 and 0 - 360, to set the moving degrees of a clock...
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+};
+
+// setTime();
+setInterval(setTime, 1000)
